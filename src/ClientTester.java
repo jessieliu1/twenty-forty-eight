83b08f9ae62a359cb03de4b067e59046e305e70a
jessieliu1 <jliu997@gmail.com>
2016-08-05 14:28:46 -0400

    Can choose to play multiple times, timing for the moves is a little off

--- src/ClientTester.java
+++ src/ClientTester.java
@@ -4,9 +4,11 @@ public class ClientTester
 	public static void main(String args[])
 	{
 		//parameters are the IP address and the port number
-		GameClient gc = new GameClient("Jessies-Macbook-Pro.local",6174);
+		GameClient gc = new GameClient("Jessies-Macbook-Pro.local",6174,
+														new RandomThinker());
 		gc.startRunning();
-		GameClient gc2 = new GameClient("Jessies-Macbook-Pro.local",6174);
-		gc2.startRunning();
+//		GameClient gc2 = new GameClient("Jessies-Macbook-Pro.local",6174, 
+//														new RandomThinker());
+//		gc2.startRunning();
 	}
 }
--- src/GameThread.java
+++ src/GameThread.java
@@ -25,51 +25,66 @@ public class GameThread extends Thread
     		out.println("Hello " + player.getName() + ", would you like to play? (Y/N)");
     		if (in.readLine().equalsIgnoreCase("Y"))
     		{
+    			out.println("How many games? Please enter a number between 1 and 100");
+    			int numberOfGames = Integer.parseInt(in.readLine());
     			out.println("Please enter your NetID: ");
     			player.setName(in.readLine().trim());
-	    		Game g = new Game(player);
-	    		//TODO: set up to ask to play more games
-	    		
-	    		//replace the line breaks with ":" because client only reads
-	    		//in one line at a time
-				out.println(g.getBoard().toString().replace("\n", ";"));
-				
-				System.out.println("Sent first board");
-		
-				while(!g.isGameOver())
-	    		{
-	    			try 
-	    			{
-	    				String input = in.readLine();
-	    				System.out.println(player.getName() + ": " + input);
-	    				GameBoard gb = g.playNextMove(input.trim());
-//	    				try {
-//	    					Thread.sleep(12000);
-//	    				} catch (InterruptedException e1) 
-//	    				{
-//	    					e1.printStackTrace();
-//	    				}
-	    				if (gb != null)
-	    				{
-	    					out.println(gb.toString().replace("\n", ";"));
-	    				}
-	    				
-					} 
-	    			catch (InvalidMoveException e) 
-	    			{
-						e.printStackTrace();
-					}
+    			
+    			for (int i = 0; i < numberOfGames; i++)
+    			{
+		    		Game g = new Game(player);
+	
+		    		
+		    		//replace the line breaks with ";" because client only reads
+		    		//in one line at a time
+					out.println(g.getBoard().toString().replace("\n", ";"));
+					
+					System.out.println("Sent first board");
+			
+					while(!g.isGameOver())
+		    		{
+		    			try 
+		    			{
+		    				String input = in.readLine();
+		    				System.out.println(player.getName() + ": " + input);
+		    				GameBoard gb = g.playNextMove(input.trim());
+	//	    				try {
+	//	    					Thread.sleep(12000);
+	//	    				} catch (InterruptedException e1) 
+	//	    				{
+	//	    					e1.printStackTrace();
+	//	    				}
+		    				if (gb != null)
+		    				{
+		    					if (gb.moreMoves())
+		    					{
+		    						out.println(gb.toString().replace("\n", ";"));
+		    					}
+		    					else 
+		    					{
+		    						
+		    						out.println(gb.toString().replace("\n", ";") 
+		    								+ ";Game Over! Score was " + g.getScore());
+		    					}
+		    				}
+		    				
+						} 
+		    			catch (InvalidMoveException e) 
+		    			{
+							e.printStackTrace();
+						}
+		    		}
 	    		}
-				out.println("Game Over! Score was " + g.getScore());
     		}
-
+    		out.println("Bye!");
 		
 			//move break if you wanna play more games
 
-//    		in.close();
-//    		out.close();
-//    		socket.close();
+    		in.close();
+    		out.close();
+    		socket.close();
     	}
+    	
     	//when the client exits, a NullPointerException will be thrown
     	//when this happens, stop the thread.
     	catch (NullPointerException np)
