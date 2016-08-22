1ab137cc83919fd7642e615b8a2c0b24835efbeb
jessieliu1 <jliu997@gmail.com>
2016-08-22 12:54:49 -0400

    Setting up multiple views

--- gsite/app/main.jsx
+++ gsite/app/main.jsx
@@ -1,11 +1,27 @@
 var React = require("react");
 var ReactDOM = require("react-dom");
+var Router = require("react-router").Router
+var Route = require('react-router').Route
+var Link = require('react-router').Link
+
+
 var GamesList = require("./components/GamesList.jsx");
 
 var _games = [{game_id: 001, score: 12},
                 {game_id: 1.01, score: 5}];
                 
 function render(){
-    ReactDOM.render(<GamesList games={_games} />, document.getElementById("container"));    
+	var destination = document.getElementById("container")
+
+	ReactDOM.render(
+		<Router>
+			<Route path= component={}>
+			</Route>
+		</Router>,
+		destination
+	);   
+
+
+
 }
 render();
--- gsite/server/server.js
+++ gsite/server/server.js
@@ -3,6 +3,24 @@ var path = require("path");
 
 var app = express();
 app.use(express.static(path.join(__dirname,"../app/dist")));
+
+
+// var mysql = require('mysql')
+
+// var connection = mysql.createConnection({
+// 	host: 'localhost',
+// 	user: 'root',
+// 	password: 'bl00p',
+// 	database: 'tester'
+// })
+
+// connection.connect(function(err) {
+// 	if (err) throw err
+// 		console.log('Connection established')
+// })
+
+
+
 app.listen(3000,function(){
     console.log("Started listening on port", 3000);
 })
